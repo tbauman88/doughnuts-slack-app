d7c439af329f566f5f83a75bb4d1484bf668336a
Tyler Bauman <t.bauman@vehikl.com>
2020-06-04 13:02:06 -0400

    updates doughnuts function

--- functions/doughnuts.js
+++ functions/doughnuts.js
@@ -1,55 +1,35 @@
 const { JSDOM } = require('jsdom');
-const chromium = require('chrome-aws-lambda');
 const fetch = require('node-fetch').default;
 
 const getDoughnuts = async () => {
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless
-  });
+  const content = await fetch(
+    'https://boxcardonuts.ca/product-category/donuts/our-weekly-flavours?orderby=price'
+  ).then((res) => res.text());
 
-  const page = await browser.newPage();
-  let domainUrl =
-    'https://boxcardonuts.ca/product-category/donuts/our-weekly-flavours';
-  await page.goto(domainUrl, { waitUntil: 'networkidle2' });
-
-  const content = await page.content();
   const dom = new JSDOM(content);
 
   const [, ...doughnuts] = Array.from(
     dom.window.document.querySelectorAll('.product_cat-our-weekly-flavours')
-  ).map((node) => ({
-    url: node.querySelector(`a[href]`).getAttribute('href'),
-    id: '',
-    name: '',
-    description: '',
-    price: '',
-    imageUrl: ''
-  }));
+  ).map((node) => {
+    const name = node.querySelector('.woocommerce-loop-product__title')
+      .textContent;
+    return {
+      url: node.querySelector(`a[href]`).getAttribute('href'),
+      name,
+      id: name.replace(/\s+/g, '-').toLowerCase(),
+      price: `$${node.querySelector('.amount').innerHTML.split('</span>')[1]}`,
+      description: '',
+      imageUrl: node
+        .querySelector('.woocommerce-loop-product__link img')
+        .getAttribute('src')
+    };
+  });
 
   await Promise.all(
     doughnuts.map(async (doughnut) => {
-      const doughnutPage = await browser.newPage();
-
-      await doughnutPage.goto(doughnut.url);
-
-      const content = await doughnutPage.content();
+      const content = await fetch(doughnut.url).then((res) => res.text());
       const dom = new JSDOM(content);
       const pageClass = '.woocommerce-product-';
-
-      const title = dom.window.document.querySelector('.product_title')
-        .innerHTML;
-
-      doughnut.id = title.replace(/\s+/g, '-').toLowerCase();
-      doughnut.name = title;
-
-      doughnut.price = `$${
-        dom.window.document
-          .querySelector('.amount')
-          .innerHTML.split('</span>')[1]
-      }`;
 
       const description = dom.window.document.querySelector(
         `${pageClass}details__short-description > p`
@@ -64,17 +44,8 @@ const getDoughnuts = async () => {
               .split('&nbsp;')
               .join('')
               .replace('  ', ' ');
-
-      doughnut.imageUrl = dom.window.document
-        .querySelector(`${pageClass}gallery__image`)
-        .querySelector(`a[href]`)
-        .getAttribute('href');
-
-      await doughnutPage.close();
     })
   );
-
-  await browser.close();
 
   return doughnuts;
 };
@@ -87,12 +58,12 @@ exports.handler = async function (event, context, callback) {
     };
   }
 
-  const url = `https://hooks.slack.com/services/${process.env.TESTING}`;
-
-  const doughnuts = await getDoughnuts();
-
   try {
-    await fetch(url, {
+    const doughnuts = await getDoughnuts();
+
+    const url = `https://hooks.slack.com/services/${process.env.VEHIKL_SOCIAL}`;
+
+    const res = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         blocks: [
@@ -109,7 +80,7 @@ exports.handler = async function (event, context, callback) {
                 type: 'section',
                 text: {
                   type: 'mrkdwn',
-                  text: 'This weeks *ARRAY* of doughnuts are:'
+                  text: '`Array.sort()` this weeks doughnuts by price'
                 }
               },
           { type: 'divider' },
@@ -132,13 +103,7 @@ exports.handler = async function (event, context, callback) {
       })
     });
 
-    callback(null, {
-      statusCode: 204,
-      body:
-        doughnuts.length === 0
-          ? 'No doughnuts were posted!'
-          : 'Successfully posted doughnuts!'
-    });
+    callback(null, { statusCode: 204, body: 'Success' });
   } catch (e) {
     callback(null, { statusCode: 500, body: 'Internal Server Error: ' + e });
   }
